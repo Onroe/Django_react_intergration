27c5b294d24d17feb4853ad8f5ca27e3fb6c2ebe
Onroe <leninkaha@gmail.com>
2022-03-10 19:34:37 +0300

    'initial'

---
+++ members/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Member(models.Model):
+    name = models.CharField("Name", max_length=240)
+    email = models.EmailField()
+    document = models.CharField("Document", max_length=20)
+    contact = models.CharField(max_length=20)
+    registrationDate = models.DateField("Registration Date", auto_now_add=True)
+
+    def __str__(self):
+        return self.name
